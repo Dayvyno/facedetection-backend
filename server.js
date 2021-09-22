aa59e2a53db77e7ca9fae611d92c0dda3b43522e
unknown <you@example.com>
2021-09-22 04:55:26 +0100

    Debugging deployment

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 app.use(cors())
 
-app.get('/', (req, res)=>{res.send("App is working")})
+app.get('/', (req, res)=>{
+  res.send("App is working")
+})
 
 
 app.listen(process.env.PORT || 2000, ()=>{console.log(`Server is running on port ${process.env.PORT}`)})
