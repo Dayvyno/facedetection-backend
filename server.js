a187bb18d501f9a5d6daf5f79b6e350696f988e9
unknown <you@example.com>
2021-09-22 04:18:03 +0100

    Testing deployment to heroku

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+app.use(cors())
+
+app.get('/', (req, res)=>{res.send("App is working")})
+
+
+app.listen(process.env.PORT || 2000, ()=>{console.log(`Server is running on port ${process.env.PORT}`)})
